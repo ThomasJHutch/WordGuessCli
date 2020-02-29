7a9fd6c40fad1e7ce1932f8d8a10d4d87e55224e
Tom Hutcheson <thomasjhutch@gmail.com>
2020-02-29 17:01:13 -0600

    completed the game

---
+++ index.js
@@ -0,0 +1,94 @@
+//NPM modules to obtain player input (letter guesses) and to add color to the display
+var Word = require("./word.js");
+var inquirer = require('inquirer');
+var colors = require('colors');
+
+wordList = ["LUKE", "ANAKIN", "DARTH VADER", "OBI WAN KENOBI", "HAN SOLO", "CHEWBACCA", "PADME", "YODA", "BOBA FETT", "JAR JAR BINKS", "R2-D2", "MACE WINDU", "DARTH MAUL", "C-3PO"];
+var select = 0;
+var chosenWord = "";
+var gameWord = "";
+var counter = 0;
+
+//Chooses a word from the word array, uses the word constructor to create the proper display and functionality;
+//'chosenWord' is used for comparison later to check if the word is solved
+function startGame() {
+    if (wordList.length < 2) {
+        wordList = ["LUKE", "ANAKIN", "DARTH VADER", "OBI WAN KENOBI", "HAN SOLO", "CHEWBACCA", "PADME", "YODA", "BOBA FETT", "JAR JAR BINKS", "R2-D2", "MACE WINDU", "DARTH MAUL", "C-3PO"];
+    }
+    select = Math.floor(Math.random() * wordList.length);
+    chosenWord = wordList[select];
+    gameWord = new Word(chosenWord);
+    gameWord.makeWord();
+    if (select > -1) {
+        wordList.splice(select, 1);
+    }
+    console.log("\nYou have 8 letter guesses to find the Star Wars character.\n".cyan)
+    promptUser();
+}
+
+//Allows the user to input a letter guess, restarts the game if player is out of wrong guesses.
+function promptUser() {
+    if (counter < 8) {
+        console.log(gameWord.showWord());
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "letter",
+                message: "\nPick a letter and press enter. ".cyan
+            }
+        ]).then(function (data) {
+            checkAnswer(data);
+        });
+    }
+    else {
+        console.log("\nYou have failed me for the last time.\n".inverse);
+        console.log(chosenWord.rainbow);
+        chosenWord = "";
+        gameWord = "";
+        select = 0;
+        counter = 0;
+        startGame();
+    }
+}
+
+//checks that the user's input is in correct format and compares the letter to gameWord to see if guess is correct
+function checkAnswer(data) {
+    if ((data.letter.length === 1) && /^[a-zA-Z]+$/.test(data.letter)) {
+        var checkable = data.letter.toUpperCase();
+        var temp = gameWord.showWord();
+        gameWord.checkGuess(checkable);
+        if (temp === gameWord.showWord()) {
+            console.log("\nSorry, wrong letter!\n".yellow);
+            counter++;
+            console.log(((8 - counter) + " guesses remaining").yellow);
+            promptUser();
+        }
+        else {
+            rightGuess();
+        }
+    }
+    else {
+        console.log("\nPlease enter a letter, one at a time.\n".yellow);
+        promptUser();
+    }
+}
+
+//If the user's guess is correct, the word array displays the word with the guessed letter(s), 
+//If the entire word is correct (filled in), the game restarts.
+function rightGuess() {
+    console.log("\nYou guessed correctly.\n".green);
+    if (chosenWord.replace(/ /g, "") == (gameWord.showWord()).replace(/ /g, "")) {
+        console.log(gameWord.showWord().america);
+        console.log('\nThe force is strong with you.\n'.america);
+        chosenWord = "";
+        gameWord = "";
+        select = 0;
+        counter = 0;
+        startGame();
+    }
+    else {
+        promptUser();
+    }
+}
+
+startGame();
